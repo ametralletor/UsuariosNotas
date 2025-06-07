04b2ff9332e69df897ab905413257fa6c4bd3fb0
ametralletor <adaysg14@gmail.com>
2025-06-07 16:35:57 +0100

    Primer commit

---
+++ src/main/java/com/proyecto/NotasUsuarios/NotasUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.NotasUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotasUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/NotasUsuarios/NotasUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.NotasUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
